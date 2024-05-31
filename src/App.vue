c50cbcd23da256e85e2a5421b98695d354844cee
kelthoumdjebbour <djebbourkelthoum@gmail.com>
2024-05-31 12:38:10 +0200

    footer component ready

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <div><!--  -->
+    <h1 class="text-3xl font-bold underline bg-red-100">
+    Hello world!
+  </h1>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <Footer />
+</template>
+
+
---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+    <footer>
+      <!-- Contenido del pie de página -->
+      <div  class="flex justify-between items-center p-4 wi">
+        <div class="dereccion">
+        <p>Direccion <br>
+        Izarza Bledea,48 <br> 48150 Sondika
+        </p></div>
+        <div class="img-log">
+        <img src="/src/assets/imagen1.png" alt="">
+      </div>
+      <div class="horario">
+         <p>Horario <br>
+        Martes a Domingo <br>
+      13:00 a 16.00 <br>
+    Sábados <br>
+  21:00 a 23:00</p></div>
+      </div>
+      <p>Este  footer.</p>
+    </footer>
+  </template>
+  
+  <script setup>
+
+
+  </script>
+  
+  <style scoped>
+  /* Estilos específicos del componente Footer */
+  footer {
+    background-color: #635d5d;
+    padding: 20px;
+    text-align: center;
+  }
+  </style>
+  
