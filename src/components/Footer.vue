2bb8e59ce8b925757cafdc0a8f9cddc80b624c30
kelthoumdjebbour <djebbourkelthoum@gmail.com>
2024-06-01 18:19:16 +0200

    añadir cambios a footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,36 @@
 <template>
     <footer>
       <!-- Contenido del pie de página -->
-      <div  class="flex justify-between items-center p-4 wi">
+      <div  class=" flex justify-between items-center h-40 bg-gray-500">
         <div class="dereccion">
         <p>Direccion <br>
-        Izarza Bledea,48 <br> 48150 Sondika
-        </p></div>
-        <div class="img-log">
-        <img src="/src/assets/imagen1.png" alt="">
+        Izarza Bledea,48 <br> 48150 Sondika</p>
+        </div>
+        <div class="img-log ">
+        <img src="/src/assets/imagen1.png" alt="" class="img-zaraza">
       </div>
       <div class="horario">
          <p>Horario <br>
-        Martes a Domingo <br>
-      13:00 a 16.00 <br>
-    Sábados <br>
-  21:00 a 23:00</p></div>
+            Martes a Domingo <br>
+            13:00 a 16.00 <br>
+            Sábados <br>
+            21:00 a 23:00</p>
+     </div>
       </div>
+      <div class="rede-social bg-red-800">
+      <h2>Visita nuestras redes Sociales</h2>
+      <div class="social-icons flex space-x-4 justify-center items-center">
+        <font-awesome-icon :icon="['fab', 'x-twitter']" class="text-darck-500 text-2xl" /> 
+        <font-awesome-icon :icon="['fab', 'facebook']" class="text-blue-600 text-2xl" />
+        <font-awesome-icon :icon="['fab', 'instagram']" class="text-pink-500 text-2xl" />
+      </div>
+      <div class="penascal-colaboraction  sm:-ml-2 flex justify-center items-center">
+        <p>Izarza es una empresa de Peñascal Kooperativa</p>
+        <a href="https://www.grupopenascal.com/">
+        <img src="../assets/penascal-logo-1 6.png" alt="" class="penascal">
+      </a>
+      </div>
+    </div>
       <p>Este  footer.</p>
     </footer>
   </template>
@@ -28,9 +43,32 @@
   <style scoped>
   /* Estilos específicos del componente Footer */
   footer {
-    background-color: #635d5d;
-    padding: 20px;
+ /*    background-color: #635d5d; */
+   /*  padding: 20px; */
     text-align: center;
   }
+.img-zaraza{
+  width: auto;
+  height: 10rem;
+}
+.horario{ margin-top: 30px;
+  margin-right: 20rem;
+}
+.dereccion{
+  margin-left: 20rem;
+}
+.dereccion p{
+  font-size:16px;
+}
+.horario p{
+  font-size: 16px;
+}
+.penascal{
+  width: 8rem;
+  height: 2rem;
+  margin-bottom: 20px;
+  margin-left: 20px;
+}
+
   </style>
   
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import { createApp } from 'vue';
-
-import './assets/tailwind.css';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+// Importa las dependencias de Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faXTwitter, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'; // Importa el icono de X (anteriormente Twitter)
+
+// Importa los estilos de Tailwind CSS
+import './assets/tailwind.css';
+
+// Añade los iconos a la librería
+library.add(faXTwitter, faFacebook, faInstagram); // Añade el icono de X (anteriormente Twitter) a la librería
+
+const app = createApp(App);
+
+// Registra el componente FontAwesomeIcon globalmente
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+app.mount('#app');
+
+
