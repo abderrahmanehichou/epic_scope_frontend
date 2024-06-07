c7562932244bcb03cc1c5c4dc960276a33dc6728
abderrahmane hichou <abderrahmanehichou06@gmail.com>
2024-06-07 13:45:54 +0200

    carousel

--- src/views/EventosView.vue
+++ src/views/EventosView.vue
@@ -1,16 +1,15 @@
 <template>
     <div>
       <!--   <Header /> -->
-      <h1 class="text-3xl font-bold underline bg-blue-100">Eventos</h1>
       <!-- Agrega aquí los componentes específicos para la vista de Eventos -->
     <!--   <Footer /> -->
-  
+    <Eventos />
     </div>
   </template>
   
   <script setup>
 /*   import Footer from '../components/Footer.vue';
   import Header from '../components/Header.vue'; */
- 
+ import Eventos from '../components/Eventos.vue';
   </script>
   
