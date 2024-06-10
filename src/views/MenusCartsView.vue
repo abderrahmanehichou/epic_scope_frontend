0f211a7f60cfefb547b39ed514a768872efb171a
l6898 <espinolalourdes83@gmail.com>
2024-06-10 10:22:26 +0200

    elimino cardSingle

--- src/views/MenusCartsView.vue
+++ src/views/MenusCartsView.vue
@@ -1,38 +1,21 @@
 <template>
   <div>
-<<<<<<< HEAD
-   <DecMenusCartas />
-   <MenuDiario/>
-   <MenuEspecial/>
-   <MenuCarta/>
-
- <CondicionMenus />
-=======
     
  <cardSingle/>
  <cardSingle/>
  <cardSingle/>
 
->>>>>>> lourdes
   </div>
 </template>
 
 <script setup>
 import{ref}from "vue";
-<<<<<<< HEAD
-import DecMenusCartas from'../components/DecMenusCartas.vue';
-import CondicionMenus from'../components/CondicionMenus.vue';
-import MenuDiario from "../components/MenuDioario.vue";
-import MenuEspecial from '../components/MenuEspecial.vue';
-import MenuCarta from '../components/MenuCarta.vue';
-=======
-import cardSingle from '../components/cardSingle.vue';
+
 import { useLink } from "vue-router";
 
 
   
 
->>>>>>> lourdes
 </script>
 
   
